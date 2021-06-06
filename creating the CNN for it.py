9a19ac1fc091db7e190d22157a4e8fdf84692256
8ahmedanwer8 <84689304+8ahmedanwer8@users.noreply.github.com>
2021-06-06 05:03:40 -0400

    Create creating the CNN for it.py

---
+++ creating the CNN for it.py
@@ -0,0 +1,42 @@
+import pickle 
+import time 
+import os 
+import cv2
+import random 
+import matplotlib.pyplot as plt 
+import numpy as np 
+from tensorflow.keras.models import Sequential 
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten, Activation, Dropout
+from tensorflow.keras.callbacks import TensorBoard
+
+NAME = f'plant leaf good or bad predictor-{int(time.time())}'
+poo = TensorBoard(log_dir=f'./logs')
+
+pickle_in = open('X.pkl', 'rb')
+X = pickle.load(pickle_in)
+
+pickle_in = open('Y.pkl', 'rb')
+Y = pickle.load(pickle_in)
+
+
+model = Sequential()
+
+model.add(Conv2D(64,(3,3), activation = 'relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Conv2D(64,(3,3), activation = 'relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Conv2D(64,(3,3), activation = 'relu'))
+model.add(MaxPooling2D(2,2))
+
+model.add(Flatten())
+
+model.add(Dense(128, input_shape = X.shape[1:], activation = 'relu'))
+model.add(Dense(15,activation = 'softmax'))
+# model.add(Dropout(rate = 0.1,seed=100)) gotta learn how to use this
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics = ['accuracy'])
+model.fit(X,Y, epochs =6, validation_split = 0.1) #lower epochs had higher val_acc i think but ill have to see
+
+model.save('64x3-CNN.model')
